43b7b0531240bc8c4aaefcc3f23162924084cbef
musab1907 <145965769+musab1907@users.noreply.github.com>
2025-05-10 10:43:55 +0300

    DevOps:Oyundaki sistem güncellemsi

---
+++ Assets/Scripts/CameraSetup.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraSetup : MonoBehaviour
+{
+    public CinemachineVirtualCamera virtualCam;
+    public GameObject player;
+
+    void Start()
+    {
+        virtualCam.Follow = player.transform;
+        virtualCam.LookAt = player.transform;
+    }
+}
+
